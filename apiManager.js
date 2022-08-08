009784f382cd02325bd2057f9a7ad72b5f7bcc83
Andricknight <64166322+Andricknight@users.noreply.github.com>
2022-08-08 12:28:45 -0500

    Add files via upload

--- apiManager.js
+++ apiManager.js
@@ -1,25 +1,25 @@
 async function uploadFile(){
 
-
+    const configJson = '{"type" : "transcription", "transcription_config" : "transcription_config", { "language": "en", "additional_vocab": [ "Cisco" ]}';
 
     let formData = new FormData();
     formData.append("data_file", fileupload.files[0]);
-    formData.append("config", fetch("https://andricknight.github.io/subbot/config.json"));
+    formData.append("config", configJson);
 
     console.log([...formData]);
 
-    // fetch('https://asr.api.speechmatics.com/v2/jobs/', {
-    //     method: "POST",
-    //     headers: {
+    fetch('https://asr.api.speechmatics.com/v2/jobs/', {
+        method: "POST",
+        headers: {
 
-    //         'Authorization':'Bearer cyBZgIZfyqIPNDlkqHHHcbBQYhwhY4FZ'
+            'Authorization':'Bearer cyBZgIZfyqIPNDlkqHHHcbBQYhwhY4FZ'
 
-    //     },
-    //     body: formData,
+        },
+        body: formData,
         
         
-    // })
-    // .then(res => res.json())
-    // .then(data => console.log(data))
-    // .catch(err => console.log(err));
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
 }
